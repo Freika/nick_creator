8b9a956740de95f26e5bf6e6fd231285934363e9
Freika <iamfrey@gmail.com>
2016-01-13 17:12:11 +0600

    Update app config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,6 @@ ActiveRecord::Migration.maintain_test_schema!
 Capybara.javascript_driver = :webkit
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
