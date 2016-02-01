40539f8c866b40d7bf9864be8d431cdcd185c66a
Freika <iamfrey@gmail.com>
2016-02-01 07:03:14 +0600

    Add linked file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:Freika/nick_creator.git'
 
 set :deploy_to, '/var/www/nick_creator'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :rbenv_path, '/opt/rbenv/'
