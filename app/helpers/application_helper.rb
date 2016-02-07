0cff401cc6d095c87739abd4197bad0c5df7128c
Freika <iamfrey@gmail.com>
2016-02-07 11:12:36 +0600

    Add favicons, logo and update navbar brand.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active_game(game)
     'active' if params[:game_id] == game
   end
+
+  def title(page_title)
+    content_for :title, page_title.to_s
+  end
+
+  def description(page_description)
+    content_for :description, page_description.to_s
+  end
 end
