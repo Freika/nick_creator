4a344debdfe451736c00973f8de9880f37bfd208
Freika <iamfrey@gmail.com>
2016-02-07 10:37:50 +0600

    Update bundle.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,13 +4,13 @@ set :branch, "master"
 # Это используется в виртуальных хостах Nginx для указания, на каких доменах
 # должно появляться приложение. Если вы еще не настроили DNS, вам будет
 # необходимо создать записи в вашем локальном файле Hosts для тестирования
-set :server_name, "95.213.204.149"
+set :server_name, "rpname.xyz"
 
 # используется в случае, если мы деплоим несколько версий одного приложения
 # по-соседству. Также предоставляет быструю проверку корректности путей файлов.
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server '95.213.204.149', user: 'deploy', roles: %w{web app db}, primary: true
+server '95.213.191.109', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
